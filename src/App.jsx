c8e6083d06418939fe57b942264802d3605de560
Morgana Ramos <morganaramos@edu.unifor.br>
2023-08-28 15:00:37 -0300

    criando projeto inicial em react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div> bem vindo</div>
+       
+    </>
+  )
+}
+
+export default App
