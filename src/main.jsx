d9fe089186ce96dafd343b6286824c63857083fe
Morgana Ramos <morganaramos@edu.unifor.br>
2023-08-29 14:32:49 -0300

    criação de rotas e page error

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Error } from './Pages/Error.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/usuario",
+    element: <Usuario/>,
+    errorElement: <Error/>,
+  },
+  {
+    path: "/cadastro",
+    element: <Cadastro/>,
+    errorElement: <Error/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
